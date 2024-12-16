93fb49c3eecb2485ddde5e01733a39a34c9e86d3
Matthew Herber <matthewherber@sbcglobal.net>
2024-12-15 22:17:47 -0500

    Add error state

--- VMixOSCProxy/Config.cs
+++ VMixOSCProxy/Config.cs
@@ -6,58 +6,61 @@ namespace ConfigXML
     public class Vmix
     {
         [XmlElement(ElementName = "ip")]
-        public string Ip { get; set; }
+        public required string Ip { get; set; }
 
         [XmlElement(ElementName = "port")]
         public int Port { get; set; }
 
         [XmlElement(ElementName = "username")]
-        public string Username { get; set; }
+        public required string Username { get; set; }
 
         [XmlElement(ElementName = "password")]
-        public string Password { get; set; }
+        public required string Password { get; set; }
 
         [XmlElement(ElementName = "tally")]
-        public string Tally { get; set; }
+        public required string Tally { get; set; }
     }
 
     [XmlRoot(ElementName = "osc")]
     public class Osc
     {
         [XmlElement(ElementName = "ip")]
-        public string Ip { get; set; }
+        public required string Ip { get; set; }
 
         [XmlElement(ElementName = "port")]
         public int Port { get; set; }
 
         [XmlElement(ElementName = "parameters")]
-        public Parameters Parameters { get; set; }
+        public required Parameters Parameters { get; set; }
     }
 
     [XmlRoot(ElementName = "parameters")]
     public class Parameters
     {
         [XmlElement(ElementName = "preview")]
-        public string Previewparameter { get; set; }
+        public required string Previewparameter { get; set; }
 
         [XmlElement(ElementName = "program")]
-        public string Programparameter { get; set; }
+        public required string Programparameter { get; set; }
 
         [XmlElement(ElementName = "standby")]
-        public string Standbyparameter { get; set; }
+        public required string Standbyparameter { get; set; }
 
         [XmlElement(ElementName = "heartbeat")]
-        public string Heartbeatparameter { get; set; }
+        public required string Heartbeatparameter { get; set; }
+
+        [XmlElement(ElementName = "error")]
+        public required string Errorparameter { get; set; }
     }
 
     [XmlRoot(ElementName = "config")]
     public class Config
     {
         [XmlElement(ElementName = "vmix")]
-        public Vmix Vmix { get; set; }
+        public required Vmix Vmix { get; set; }
 
         [XmlElement(ElementName = "osc")]
-        public Osc Osc { get; set; }
+        public required Osc Osc { get; set; }
 
         [XmlElement(ElementName = "updaterate")]
         public int UpdateRate { get; set; }
--- VMixOSCProxy/Program.cs
+++ VMixOSCProxy/Program.cs
@@ -14,10 +14,12 @@ public static class Program
     private static UdpClient oscClient = new();
     private static Config config;
     private static bool Heartbeat = false;
+    private static bool Error = false;
+    const string avatarParamPrefix = "/avatar/parameters/";
 
     public static async Task Main()
     {
-        //load in the config file
+        //load in the config file'
         string xml = await File.ReadAllTextAsync("config.xml");
 
         XmlSerializer serializer = new XmlSerializer(typeof(Config));
@@ -43,10 +45,16 @@ public static class Program
         oscClient = new(config.Osc.Ip, config.Osc.Port);
 
         // Create a timer
-        System.Timers.Timer myTimer = new System.Timers.Timer();
-        myTimer.Elapsed += new ElapsedEventHandler(myEventAsync);
-        myTimer.Interval = config.UpdateRate;
-        myTimer.Start();
+        System.Timers.Timer mainTimer = new System.Timers.Timer();
+        mainTimer.Elapsed += new ElapsedEventHandler(WatchVMIX);
+        mainTimer.Interval = config.UpdateRate;
+        mainTimer.Start();
+
+        //setup a seperate timer that always has the same update rate for the heartbeat
+        System.Timers.Timer heartbeatTimer = new System.Timers.Timer();
+        heartbeatTimer.Elapsed += new ElapsedEventHandler(SendProgramStatus);
+        heartbeatTimer.Interval = 100;
+        heartbeatTimer.Start();
 
         //wait infinitely
         await Task.Delay(-1);
@@ -60,69 +68,137 @@ public static class Program
         Standby
     }
 
-    public static async void myEventAsync(object source, ElapsedEventArgs e)
+    public static async void SendProgramStatus(object source, ElapsedEventArgs e)
     {
-        //request VMix XML
-        string xml = await vmixclient.GetStringAsync("");
-
-        //replace all "False" with "false" and "True" with "true"
-        xml = xml.Replace("False", "false").Replace("True", "true");
-
-        XmlSerializer serializer = new XmlSerializer(typeof(VMixAPI.Vmix));
-        using (StringReader reader = new StringReader(xml))
+        try
         {
-            VMixAPI.Vmix vmix = (VMixAPI.Vmix)serializer.Deserialize(reader);
-
-            //clear console
-            Console.Clear();
-
-            //print VMix version
-            Console.WriteLine($"VMix version: {vmix.Version}");
-
-            //try to find the input with the name the user entered, otherwise print an error and skip everything else
-            Input input = vmix.Inputs.Input.FirstOrDefault(i => i.Title == config.Vmix.Tally);
-            if (input == null)
-            {
-                Console.WriteLine($"Input with name '{config.Vmix.Tally}' not found");
-                return;
-            }
-            Console.WriteLine($"Your Tally is: {input.Title}");
-
-            //print current in preview and in active
-            Console.WriteLine($"Current {VMixState.Preview} in VMix: {vmix.PreviewInput.Title}");
-            Console.WriteLine($"Current {VMixState.Live} in VMix: {vmix.ActiveInput.Title}");
-
-            //determine if we are live, preview or standby
-            VMixState state;
-            if (vmix.Active == input.Number) //this needs to be first to ensure that if you are both active and previewed, that you are considered live
-            {
-                state = VMixState.Live;
-            }
-            else if (vmix.Preview == input.Number)
-            {
-                state = VMixState.Preview;
-            }
-            else
-            {
-                state = VMixState.Standby;
-            }
-            Console.WriteLine($"You are currently: {state}");
-
-            //setup endpoints
-            const string avatarParamPrefix = "/avatar/parameters/";
-            Address preview = new(avatarParamPrefix + config.Osc.Parameters.Previewparameter);
-            Address program = new(avatarParamPrefix + config.Osc.Parameters.Programparameter);
-            Address standby = new(avatarParamPrefix + config.Osc.Parameters.Standbyparameter);
             Address heartbeat = new(avatarParamPrefix + config.Osc.Parameters.Heartbeatparameter);
-
-            //send OSC updates
-            await SendOSC(preview, BoolToValue(state == VMixState.Preview));
-            await SendOSC(program, BoolToValue(state == VMixState.Live));
-            await SendOSC(standby, BoolToValue(state == VMixState.Standby));
-            //flip heartbeat
             Heartbeat = !Heartbeat;
             await SendOSC(heartbeat, BoolToValue(Heartbeat));
+
+            Address error = new(avatarParamPrefix + config.Osc.Parameters.Errorparameter);
+            await SendOSC(error, BoolToValue(Error));
         }
+        catch (Exception ex)
+        {
+            //if log exception is true, throw
+            if (LogException(ex).Result)
+            {
+                throw;
+            }
+        }
+    }
+
+    public static async void WatchVMIX(object source, ElapsedEventArgs e)
+    {
+        //clear console
+        Console.Clear();
+        try
+        {
+            //request VMix XML
+            string xml = await vmixclient.GetStringAsync("");
+
+            //replace all "False" with "false" and "True" with "true"
+            xml = xml.Replace("False", "false").Replace("True", "true");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(VMixAPI.Vmix));
+            using (StringReader reader = new StringReader(xml))
+            {
+                VMixAPI.Vmix vmix = (VMixAPI.Vmix)serializer.Deserialize(reader);
+
+                //print VMix version
+                Console.WriteLine($"VMix version: {vmix.Version}");
+
+                //try to find the input with the name the user entered, otherwise print an error and skip everything else
+                Input input = vmix.Inputs.Input.FirstOrDefault(i => i.Title == config.Vmix.Tally);
+                if (input == null)
+                {
+                    Console.WriteLine($"Input with name '{config.Vmix.Tally}' not found");
+                    Error = true;
+                }
+                else
+                {
+                    Console.WriteLine($"Your Tally is: {input.Title}");
+
+                    //print current in preview and in active
+                    Console.WriteLine(
+                        $"Current {VMixState.Preview} in VMix: {vmix.PreviewInput.Title}"
+                    );
+                    Console.WriteLine(
+                        $"Current {VMixState.Live} in VMix: {vmix.ActiveInput.Title}"
+                    );
+
+                    //determine if we are live, preview or standby
+                    VMixState state;
+                    if (vmix.Active == input.Number) //this needs to be first to ensure that if you are both active and previewed, that you are considered live
+                    {
+                        state = VMixState.Live;
+                    }
+                    else if (vmix.Preview == input.Number)
+                    {
+                        state = VMixState.Preview;
+                    }
+                    else
+                    {
+                        state = VMixState.Standby;
+                    }
+                    Console.WriteLine($"You are currently: {state}");
+
+                    //setup endpoints
+                    Address preview =
+                        new(avatarParamPrefix + config.Osc.Parameters.Previewparameter);
+                    Address program =
+                        new(avatarParamPrefix + config.Osc.Parameters.Programparameter);
+                    Address standby =
+                        new(avatarParamPrefix + config.Osc.Parameters.Standbyparameter);
+
+                    //send OSC updates
+                    await SendOSC(preview, BoolToValue(state == VMixState.Preview));
+                    await SendOSC(program, BoolToValue(state == VMixState.Live));
+                    await SendOSC(standby, BoolToValue(state == VMixState.Standby));
+                    //clear error state
+                    Error = false;
+                }
+            }
+
+        }
+        catch (Exception ex)
+        {
+            //if log exception is true, throw
+            if (LogException(ex).Result)
+            {
+                throw;
+            }
+
+            //set error state
+            Error = true;
+        }
+    }
+
+    private static async Task<bool> LogException(Exception ex)
+    {
+        switch (ex)
+        {
+            case HttpRequestException:
+                Console.WriteLine(
+                    "Could not communicate to VMix over HTTP, is the IP/Port correct or is VMix running?"
+                );
+                Console.WriteLine($"VMix Configured Endpoint: {config.Vmix.Ip}:{config.Vmix.Port}");
+                break;
+            case SocketException:
+                Console.WriteLine(
+                    "Could not communicate to VRChat over OSC, is the port/IP correct or is VRChat running?"
+                );
+                Console.WriteLine(
+                    $"VRChat OSC Configured Endpoint: {config.Osc.Ip}:{config.Osc.Port}"
+                );
+                break;
+            default:
+                //return failed
+                return true;
+        }
+        //return success
+        return false;
     }
 
     public static async Task SendOSC(Address address, params object[] values)
