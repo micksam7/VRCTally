79fc1b208731807b0912648852faea76b59a0704
Happyrobot33 <matthewherber@sbcglobal.net>
2024-12-20 13:49:01 -0500

    cleanup wizard system, add helptext, add wildcard

--- VRCTallyApp/Program.cs
+++ VRCTallyApp/Program.cs
@@ -38,153 +38,183 @@ public class ProgramWindow : Window
         FrameView oscView;
         FrameView vmixView;
 
-        #region wizard
-        //create a setup wizard to edit the config
-        var vmixConnectionWizard = new Wizard("VMIX Connection");
-        var firstVmixStep = new Wizard.WizardStep("");
-        vmixConnectionWizard.AddStep(firstVmixStep);
-
-        Option updateRateField =
-            new("Update Rate: ", config.Vmix.UpdateRate.ToString()) { X = 0, Y = 0 };
-        firstVmixStep.Add(updateRateField);
-
-        Option ipField = new("IP: ", config.Vmix.Ip) { X = 0, Y = Pos.Bottom(updateRateField), };
-        firstVmixStep.Add(ipField);
-
-        Option portField =
-            new("Port: ", config.Vmix.Port.ToString()) { X = 0, Y = Pos.Bottom(ipField), };
-        firstVmixStep.Add(portField);
-
-        Option usernameField =
-            new("Username: ", config.Vmix.Username) { X = 0, Y = Pos.Bottom(portField), };
-        firstVmixStep.Add(usernameField);
-
-        Option passwordField =
-            new("Password: ", config.Vmix.Password) { X = 0, Y = Pos.Bottom(usernameField), };
-        passwordField.tf.Secret = true;
-        passwordField.tf.Enter += (e) =>
-        {
-            passwordField.tf.Secret = false;
-        };
-        passwordField.tf.Leave += (e) =>
-        {
-            passwordField.tf.Secret = true;
-        };
-        firstVmixStep.Add(passwordField);
-
-        //we need to make a step for selecting the tally
-        //query the api once
-        var tallyWizard = new Wizard("Tally Selection");
-        var tallyWizardStep = new Wizard.WizardStep("");
-        tallyWizard.AddStep(tallyWizardStep);
-        var notFoundLabel = new Label("No inputs found!") { X = 0, Y = 0, };
-        notFoundLabel.Visible = false;
-        var inputList = new ListView(new NStack.ustring[0])
-        {
-            X = 0,
-            Y = 0,
-            Width = Dim.Fill(),
-            Height = Dim.Fill()
-        };
-        inputList.AllowsMultipleSelection = false;
-        inputList.AllowsMarking = true;
-        tallyWizardStep.Add(inputList);
-        tallyWizardStep.Add(notFoundLabel);
-
-        tallyWizard.Added += async (e) =>
-        {
-            await vmix.PopulateSingleShot();
-
-            //populate the options
-            NStack.ustring[] inputs = vmix.GetInputs();
-            inputList.SetSource(inputs);
-
-            if (inputs.Length == 0)
-            {
-                notFoundLabel.Visible = true;
-                notFoundLabel.Text =
-                    $"No inputs found! Check your VMix configuration. \'{config.Vmix.Tally}\' will be used instead.";
-            }
-            else
-            {
-                notFoundLabel.Visible = false;
-
-                //we should try and select the one we are already using
-                for (int i = 0; i < inputs.Length; i++)
-                {
-                    if (inputs[i].ToString() == config.Vmix.Tally)
-                    {
-                        inputList.SelectedItem = i;
-                        inputList.MarkUnmarkRow();
-                        break;
-                    }
-                }
-            }
-        };
-
-        tallyWizard.Finished += (e) =>
-        {
-            //we need to load in everything possibly just changed in the previous step
-            NStack.ustring[] inputs = vmix.GetInputs();
-            if (inputList.SelectedItem > 0)
-            {
-                config.Vmix.Tally = inputs[inputList.SelectedItem].ToString();
-            }
-            vmix.UpdateConfig(config);
-            SaveConfig(config);
-
-            //we now want to read in what the radio label selection was
-            int selected = inputList.SelectedItem;
-            if (inputList.Source.Length > 0)
-            {
-                string potentialNewTally = vmix.GetInputs()[selected]?.ToString() ?? "";
-                //null check
-                if (!string.IsNullOrWhiteSpace(potentialNewTally))
-                {
-                    config.Vmix.Tally = potentialNewTally;
-                }
-            }
-
-            vmix.updateTimer.Start();
-
-            Remove(tallyWizard);
-        };
-
-        vmixConnectionWizard.Finished += (e) =>
-        {
-            //we need to load in everything possibly just changed in the previous step
-            config.Vmix.UpdateRate = int.Parse(updateRateField.tf.Text.ToString());
-            config.Vmix.Ip = ipField.tf.Text.ToString();
-            config.Vmix.Port = int.Parse(portField.tf.Text.ToString());
-            config.Vmix.Username = usernameField.tf.Text.ToString();
-            config.Vmix.Password = passwordField.tf.Text.ToString();
-            vmix.UpdateConfig(config);
-            SaveConfig(config);
-
-            oscView = osc.GetWindow(0, 0, Dim.Percent(50), Dim.Fill());
-            vmixView = vmix.GetWindow(Pos.Right(oscView), 0, Dim.Percent(50), Dim.Fill());
-
-            //restart the vmix timer
-            vmix.updateTimer.Start();
-
-            //remove the wizard
-            Remove(vmixConnectionWizard);
-        };
-        #endregion
-
         var RemoteTallySelectorButton = new Button("Select Existing Tally") { X = 0, Y = 0, };
         RemoteTallySelectorButton.Clicked += () =>
         {
+            var tallyWizard = new Wizard("Tally Selection");
+            var tallyWizardStep = new Wizard.WizardStep("");
+            tallyWizardStep.HelpText =
+                "Select the input you want to follow with your tally light. Scroll up/down to see more inputs.";
+            tallyWizard.AddStep(tallyWizardStep);
+            var notFoundLabel = new Label("No inputs found!") { X = 0, Y = 0, };
+            notFoundLabel.Visible = false;
+            var inputList = new ListView(new NStack.ustring[0])
+            {
+                X = 0,
+                Y = 0,
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+            inputList.AllowsMultipleSelection = false;
+            inputList.AllowsMarking = true;
+            tallyWizardStep.Add(inputList);
+            tallyWizardStep.Add(notFoundLabel);
+
+            tallyWizard.Added += async (e) =>
+            {
+                await vmix.PopulateSingleShot();
+
+                //populate the options
+                NStack.ustring[] inputs = vmix.GetInputs();
+                inputList.SetSource(inputs);
+
+                if (inputs.Length == 0)
+                {
+                    notFoundLabel.Visible = true;
+                    notFoundLabel.Text =
+                        $"No inputs found! Check your VMix configuration. \'{config.Vmix.Tally}\' will be used instead.";
+                }
+                else
+                {
+                    notFoundLabel.Visible = false;
+
+                    //we should try and select the one we are already using
+                    for (int i = 0; i < inputs.Length; i++)
+                    {
+                        if (inputs[i].ToString() == config.Vmix.Tally)
+                        {
+                            inputList.SelectedItem = i;
+                            inputList.MarkUnmarkRow();
+                            break;
+                        }
+                    }
+                }
+            };
+
+            tallyWizard.Finished += (e) =>
+            {
+                //we need to load in everything possibly just changed in the previous step
+                NStack.ustring[] inputs = vmix.GetInputs();
+                if (inputList.SelectedItem > 0)
+                {
+                    config.Vmix.Tally = inputs[inputList.SelectedItem].ToString();
+                }
+                vmix.UpdateConfig(config);
+                SaveConfig(config);
+
+                //we now want to read in what the radio label selection was
+                int selected = inputList.SelectedItem;
+                if (inputList.Source.Length > 0)
+                {
+                    string potentialNewTally = vmix.GetInputs()[selected]?.ToString() ?? "";
+                    //null check
+                    if (!string.IsNullOrWhiteSpace(potentialNewTally))
+                    {
+                        config.Vmix.Tally = potentialNewTally;
+                    }
+                }
+
+                vmix.updateTimer.Start();
+
+                Remove(tallyWizard);
+            };
             Add(tallyWizard);
             //pause the vmix timer
             vmix.updateTimer.Stop();
         };
         Add(RemoteTallySelectorButton);
 
+        var WildcardTallySelectorButton = new Button("Wildcard Tally")
+        {
+            X = Pos.Right(RemoteTallySelectorButton),
+            Y = 0,
+        };
+        WildcardTallySelectorButton.Clicked += () =>
+        {
+            //create a setup wizard to edit the config
+            var wildcardTallyWizard = new Wizard("Wildcard Tally");
+            var firstStep = new Wizard.WizardStep("");
+            firstStep.HelpText =
+                "Enter a section of a input name to automatically find the input with that string in it. For example, if you want to track a input that looks like \"Cam 4 - Happyrobot33\", you can just enter \"Happy\" and it will automatically find the highest priority input that matches. IMPORTANT: This is case-sensitive!";
+            wildcardTallyWizard.AddStep(firstStep);
+            Option wildcardField = new("Wildcard Tally: ", config.Vmix.Tally) { X = 0, Y = 0, };
+            firstStep.Add(wildcardField);
+            wildcardTallyWizard.Finished += (e) =>
+            {
+                config.Vmix.Tally = wildcardField.tf.Text.ToString();
+                vmix.UpdateConfig(config);
+                SaveConfig(config);
+
+                vmix.updateTimer.Start();
+
+                Remove(wildcardTallyWizard);
+            };
+            Add(wildcardTallyWizard);
+        };
+        Add(WildcardTallySelectorButton);
+
         //setup a button to edit the config
-        var VmixConfigButton = new Button("Edit VMix Config") { X = Pos.Right(RemoteTallySelectorButton), Y = 0, };
+        var VmixConfigButton = new Button("Edit VMix Config")
+        {
+            X = Pos.Right(WildcardTallySelectorButton),
+            Y = 0,
+        };
         VmixConfigButton.Clicked += () =>
         {
+            //create a setup wizard to edit the config
+            var vmixConnectionWizard = new Wizard("VMIX Connection");
+            var firstVmixStep = new Wizard.WizardStep("");
+            vmixConnectionWizard.AddStep(firstVmixStep);
+
+            Option updateRateField =
+                new("Update Rate: ", config.Vmix.UpdateRate.ToString()) { X = 0, Y = 0 };
+            firstVmixStep.Add(updateRateField);
+
+            Option ipField =
+                new("IP: ", config.Vmix.Ip) { X = 0, Y = Pos.Bottom(updateRateField), };
+            firstVmixStep.Add(ipField);
+
+            Option portField =
+                new("Port: ", config.Vmix.Port.ToString()) { X = 0, Y = Pos.Bottom(ipField), };
+            firstVmixStep.Add(portField);
+
+            Option usernameField =
+                new("Username: ", config.Vmix.Username) { X = 0, Y = Pos.Bottom(portField), };
+            firstVmixStep.Add(usernameField);
+
+            Option passwordField =
+                new("Password: ", config.Vmix.Password) { X = 0, Y = Pos.Bottom(usernameField), };
+            passwordField.tf.Secret = true;
+            passwordField.tf.Enter += (e) =>
+            {
+                passwordField.tf.Secret = false;
+            };
+            passwordField.tf.Leave += (e) =>
+            {
+                passwordField.tf.Secret = true;
+            };
+            firstVmixStep.Add(passwordField);
+
+            vmixConnectionWizard.Finished += (e) =>
+            {
+                //we need to load in everything possibly just changed in the previous step
+                config.Vmix.UpdateRate = int.Parse(updateRateField.tf.Text.ToString());
+                config.Vmix.Ip = ipField.tf.Text.ToString();
+                config.Vmix.Port = int.Parse(portField.tf.Text.ToString());
+                config.Vmix.Username = usernameField.tf.Text.ToString();
+                config.Vmix.Password = passwordField.tf.Text.ToString();
+                vmix.UpdateConfig(config);
+                SaveConfig(config);
+
+                oscView = osc.GetWindow(0, 0, Dim.Percent(50), Dim.Fill());
+                vmixView = vmix.GetWindow(Pos.Right(oscView), 0, Dim.Percent(50), Dim.Fill());
+
+                //restart the vmix timer
+                vmix.updateTimer.Start();
+
+                //remove the wizard
+                Remove(vmixConnectionWizard);
+            };
             Add(vmixConnectionWizard);
             //pause the vmix timer
             vmix.updateTimer.Stop();
